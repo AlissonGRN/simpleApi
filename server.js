3d43cdbe4140d30d7674dd4c726e3c6c097cca84
AlissonGRN <guilhermeribeiro099@gmail.com>
2024-02-07 19:27:07 -0300

    add server setup

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 const express = require('express')
-const 
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// Server config
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Middleware to process Json data
+
+app.use(bodyParser.json())
+
+// Routes
+
+// Server start
+
+app.listen(PORT, () => {
+    console.log(`Server Running on port: ${PORT}`)
+})
